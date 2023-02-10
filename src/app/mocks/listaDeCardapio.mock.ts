6b27616fe13294263c371dadcbe9f4a46fa53def
Felipe Santos <felipe.santos@f1rst.com.br>
2023-02-10 02:09:50 -0300

    adiciona imagens novas no cadapio

---
+++ src/app/mocks/listaDeCardapio.mock.ts
@@ -0,0 +1,157 @@
+
+export const listaCardapios = [
+    {
+        "menu": "Sessão Rosh",
+        "selected": true,
+        "itens": [
+            {
+                "img": "/assets/images/cervejas/corona-330.jpg",
+                "description": "Rosh",
+                "value": 40
+            },
+            {
+                "img": "/assets/images/cervejas/stella-330.jpg",
+                "description": "Rosh",
+                "value": 40
+            },
+            {
+                "img": "/assets/images/cervejas/heineken-330.jpg",
+                "description": "Rosh",
+                "value": 40
+            },
+            {
+                "img": "/assets/images/cervejas/budweiser-330.png",
+                "description": "Rosh",
+                "value": 40
+            }
+        ]
+    },
+    {
+        "menu": "Cervejas",
+        "selected": false,
+        "itens": [
+            {
+                "img": "/assets/images/cervejas/corona-330.jpg",
+                "description": "Cerveja Corona Extra 330ml",
+                "value": 15
+            },
+            {
+                "img": "/assets/images/cervejas/stella-330.jpg",
+                "description": "Stella Artois 330ml",
+                "value": 15
+            },
+            {
+                "img": "/assets/images/cervejas/heineken-330.jpg",
+                "description": "Heineken 330ml",
+                "value": 15
+            },
+            {
+                "img": "/assets/images/cervejas/budweiser-330.png",
+                "description": "Budweiser 330ml",
+                "value": 15
+            }
+        ]
+    },
+    {
+        "menu": "Garrafas",
+        "selected": false,
+        "itens": [
+            {
+                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "description": "Garrafa Licor 43",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-tanqueray.png",
+                "description": "Garrafa Tanqueray",
+                "value": 230
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-bombay.png",
+                "description": "Garrafa Bombay",
+                "value": 200
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-black-label.png",
+                "description": "Garrafa Black Label",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-red-label.png",
+                "description": "Garrafa Red Label",
+                "value": 190
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-gold-label-1l.JPG",
+                "description": "Garrafa Gold Label",
+                "value": 320
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-blue-label.png",
+                "description": "Garrafa Blue Label",
+                "value": 1999
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-royal-salut.png",
+                "description": "Garrafa Chivas Royal Salute 21 Anos",
+                "value": 1800
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-jack-daniels.png",
+                "description": "Garrafa Jack Daniels",
+                "value": 310
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-jack-daniels-maca.png",
+                "description": "Garrafa Jack Daniels Maça Verde",
+                "value": 320
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-jack-daniels-mel.JPG",
+                "description": "Garrafa Jack Daniels Mel",
+                "value": 320
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-ciroc.PNG",
+                "description": "Garrafa Ciroc Tradicional",
+                "value": 320
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-ciroc-red.PNG",
+                "description": "Garrafa Ciroc Red Berry",
+                "value": 320
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-ciroc-pineapple.PNG",
+                "description": "Garrafa Ciroc Pineapple",
+                "value": 320
+            },
+        ]
+    },
+    {
+        "menu": "Combos",
+        "selected": false,
+        "itens": [
+            {
+                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "description": "Combo Black Label",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "description": "Combo Royal Salute",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "description": "Combo Tanqueray",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "description": "Combo Bombay",
+                "value": 250
+            },
+        ]
+    }
+]
