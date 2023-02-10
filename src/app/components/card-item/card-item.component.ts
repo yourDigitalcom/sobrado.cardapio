34c25afc859b5ec9ec4bb965d6605a422dc85937
Felipe Santos <felipe.santos@f1rst.com.br>
2023-02-10 19:35:06 -0300

    insersao dos ite,s

--- src/app/components/card-item/card-item.component.ts
+++ src/app/components/card-item/card-item.component.ts
@@ -4,6 +4,7 @@ export class CardItem {
   public img: string;
   public description: string;
   public value: number;
+  public title?: string;
 }
 
 @Component({
--- src/app/mocks/listaDeCardapio.mock.ts
+++ src/app/mocks/listaDeCardapio.mock.ts
@@ -5,25 +5,29 @@ export const listaCardapios = [
         "selected": true,
         "itens": [
             {
-                "img": "/assets/images/cervejas/corona-330.jpg",
-                "description": "Rosh",
+                "img": "/assets/images/cardapio/marca-ziggy.png",
+                "title": "Essência Ziggy",
+                "description": "Tropical - Banana Tropical - Morango Laranja - Frutti - Melancia - Uva",
                 "value": 40
             },
             {
-                "img": "/assets/images/cervejas/stella-330.jpg",
-                "description": "Rosh",
+                "img": "/assets/images/cardapio/essencia-magic.png",
+                "title": "Essência Magic",
+                "description": "Pêssego - Goiaba",
                 "value": 40
             },
             {
-                "img": "/assets/images/cervejas/heineken-330.jpg",
-                "description": "Rosh",
+                "img": "/assets/images/cardapio/essencia-adalya.png",
+                "title": "Essência Adalya",
+                "description": "Love 66",
                 "value": 40
             },
             {
-                "img": "/assets/images/cervejas/budweiser-330.png",
-                "description": "Rosh",
+                "img": "/assets/images/cardapio/essencia-zommo.png",
+                "title": "Essência Zommo",
+                "description": "Menta",
                 "value": 40
-            }
+            },
         ]
     },
     {
@@ -32,24 +36,45 @@ export const listaCardapios = [
         "itens": [
             {
                 "img": "/assets/images/cervejas/corona-330.jpg",
-                "description": "Cerveja Corona Extra 330ml",
+                "description": "Cerveja Corona Extra 330ml UN.",
                 "value": 15
             },
+
             {
                 "img": "/assets/images/cervejas/stella-330.jpg",
-                "description": "Stella Artois 330ml",
+                "description": "Stella Artois 330ml UN.",
                 "value": 15
             },
             {
                 "img": "/assets/images/cervejas/heineken-330.jpg",
-                "description": "Heineken 330ml",
+                "description": "Heineken 330ml UN.",
                 "value": 15
             },
             {
                 "img": "/assets/images/cervejas/budweiser-330.png",
-                "description": "Budweiser 330ml",
+                "description": "Budweiser 330ml UN.",
                 "value": 15
-            }
+            },
+            {
+                "img": "/assets/images/cervejas/corona-330.jpg",
+                "description": "Balde Cerveja Corona Extra 330ml 5 UN.",
+                "value": 75
+            },
+            {
+                "img": "/assets/images/cervejas/heineken-330.jpg",
+                "description": "Balde Heineken 330ml 5 UN.",
+                "value": 75
+            },
+            {
+                "img": "/assets/images/cervejas/stella-330.jpg",
+                "description": "Balde Stella Artois 330ml 5 UN.",
+                "value": 75
+            },
+            {
+                "img": "/assets/images/cervejas/budweiser-330.png",
+                "description": "Balde Budweiser 330ml 5 UN.",
+                "value": 75
+            },
         ]
     },
     {
@@ -66,11 +91,11 @@ export const listaCardapios = [
                 "description": "Garrafa Tanqueray",
                 "value": 230
             },
-            {
-                "img": "/assets/images/cardapio/garrafa-bombay.png",
-                "description": "Garrafa Bombay",
-                "value": 200
-            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-bombay.png",
+            //     "description": "Garrafa Bombay",
+            //     "value": 200
+            // },
             {
                 "img": "/assets/images/cardapio/garrafa-black-label.png",
                 "description": "Garrafa Black Label",
@@ -86,46 +111,46 @@ export const listaCardapios = [
                 "description": "Garrafa Gold Label",
                 "value": 320
             },
-            {
-                "img": "/assets/images/cardapio/garrafa-blue-label.png",
-                "description": "Garrafa Blue Label",
-                "value": 1999
-            },
-            {
-                "img": "/assets/images/cardapio/garrafa-royal-salut.png",
-                "description": "Garrafa Chivas Royal Salute 21 Anos",
-                "value": 1800
-            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-blue-label.png",
+            //     "description": "Garrafa Blue Label",
+            //     "value": 1999
+            // },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-royal-salut.png",
+            //     "description": "Garrafa Chivas Royal Salute 21 Anos",
+            //     "value": 1800
+            // },
             {
                 "img": "/assets/images/cardapio/garrafa-jack-daniels.png",
                 "description": "Garrafa Jack Daniels",
                 "value": 310
             },
-            {
-                "img": "/assets/images/cardapio/garrafa-jack-daniels-maca.png",
-                "description": "Garrafa Jack Daniels Maça Verde",
-                "value": 320
-            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-jack-daniels-maca.png",
+            //     "description": "Garrafa Jack Daniels Maça Verde",
+            //     "value": 320
+            // },
             {
                 "img": "/assets/images/cardapio/garrafa-jack-daniels-mel.JPG",
                 "description": "Garrafa Jack Daniels Mel",
                 "value": 320
             },
-            {
-                "img": "/assets/images/cardapio/garrafa-ciroc.PNG",
-                "description": "Garrafa Ciroc Tradicional",
-                "value": 320
-            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-ciroc.PNG",
+            //     "description": "Garrafa Ciroc Tradicional",
+            //     "value": 320
+            // },
             {
                 "img": "/assets/images/cardapio/garrafa-ciroc-red.PNG",
                 "description": "Garrafa Ciroc Red Berry",
                 "value": 320
             },
-            {
-                "img": "/assets/images/cardapio/garrafa-ciroc-pineapple.PNG",
-                "description": "Garrafa Ciroc Pineapple",
-                "value": 320
-            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-ciroc-pineapple.PNG",
+            //     "description": "Garrafa Ciroc Pineapple",
+            //     "value": 320
+            // },
         ]
     },
     {
@@ -133,25 +158,98 @@ export const listaCardapios = [
         "selected": false,
         "itens": [
             {
-                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "img": "/assets/images/cardapio/garrafa-red-label.png",
+                "description": "Combo Red Label",
+                "value": 250
+            },
+            {
+                "img": "/assets/images/cardapio/garrafa-black-label.png",
                 "description": "Combo Black Label",
-                "value": 250
+                "value": 320
             },
             {
-                "img": "/assets/images/cardapio/garrafa-licor-43.png",
-                "description": "Combo Royal Salute",
-                "value": 250
+                "img": "/assets/images/cardapio/garrafa-gold-label-1l.JPG",
+                "description": "Combo Gold Label",
+                "value": 500
             },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-blue-label.png",
+            //     "description": "Combo Blue Label",
+            //     "value": 2100
+            // },
+            // {
+            //     "img": "/assets/images/cardapio/royal-salut.png",
+            //     "description": "Combo Royal Salute",
+            //     "value": 1600
+            // },
             {
-                "img": "/assets/images/cardapio/garrafa-licor-43.png",
+                "img": "/assets/images/cardapio/garrafa-tanqueray.png",
                 "description": "Combo Tanqueray",
-                "value": 250
+                "value": 300
+            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-bombay.png",
+            //     "description": "Combo Bombay",
+            //     "value": 280
+            // },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-ciroc.PNG",
+            //     "description": "Combo Ciroc Tradicional ",
+            //     "value": 320
+            // },
+            {
+                "img": "/assets/images/cardapio/garrafa-ciroc-red.PNG",
+                "description": "Combo Ciroc Red Berry",
+                "value": 340
+            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-ciroc-pineapple.PNG",
+            //     "description": "Combo Ciroc Pineapple",
+            //     "value": 420
+            // },
+            {
+                "img": "/assets/images/cardapio/garrafa-jack-daniels.png",
+                "description": "Combo Jack Daniels",
+                "value": 400
+            },
+            // {
+            //     "img": "/assets/images/cardapio/garrafa-jack-daniels-maca.png",
+            //     "description": "Combo Jack Daniels Maça Verde",
+            //     "value": 420
+            // },
+            {
+                "img": "/assets/images/cardapio/garrafa-jack-daniels-mel.JPG",
+                "description": "Combo Jack Daniels Mel ",
+                "value": 420
+            },
+        ],
+    },
+    {
+        "menu": "Não Alcoólicos",
+        "selected": false,
+        "itens": [
+            {
+                "img": "/assets/images/cardapio/agua-mineral.png",
+                "description": "Água",
+                "value": 5
             },
             {
-                "img": "/assets/images/cardapio/garrafa-licor-43.png",
-                "description": "Combo Bombay",
-                "value": 250
+                "img": "/assets/images/cardapio/red-bull-normal.png",
+                "description": "Red Bull Tradicional",
+                "value": 15
+            },
+            {
+                "img": "/assets/images/cardapio/red-bull-melancia.png",
+                "description": "Red Bull Tropical",
+                "value": 15
+            },
+            {
+                "img": "/assets/images/cardapio/red-bull-tropical.png",
+                "description": "Red Bull Melancia",
+                "value": 15
             },
         ]
     }
+
+
 ]
