102db2cc7f4a7061cd1ca32f1714ce9d97ae6ca4
Felipe Santos <felipe.santos@f1rst.com.br>
2023-11-15 19:53:31 -0300

    altera preco rosh

--- src/app/mocks/essencias.mock.ts
+++ src/app/mocks/essencias.mock.ts
@@ -3,90 +3,90 @@ export const itensEssencias =  [
         "img": "/assets/images/cardapio/essencias/7belo.png",
         // "title": "Essência Ziggy",
         "description": "7 Belo",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/banana.png",
         // "title": "Essência Ziggy",
         "description": "Banana",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/goiaba.png",
         // "title": "Essência Ziggy",
         "description": "Goiaba",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/iorgute.png",
         // "title": "Essência Ziggy",
         "description": "Iorgute",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/laranja-com-morango.png",
         // "title": "Essência Ziggy",
         "description": "Laranja Com Morango",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/love66.png",
         // "title": "Essência Ziggy",
         "description": "Love 66",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/manga.png",
         // "title": "Essência Ziggy",
         "description": "Manga",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/maracuja.png",
         // "title": "Essência Ziggy",
         "description": "Maracujá",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/melancia.png",
         // "title": "Essência Ziggy",
         "description": "Melancia",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/mente.png",
         // "title": "Essência Ziggy",
         "description": "Menta",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/pera.png",
         // "title": "Essência Ziggy",
         "description": "Pêra",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/pessego.png",
         // "title": "Essência Ziggy",
         "description": "Pêssego",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/tropical.png",
         // "title": "Essência Ziggy",
         "description": "Tropical",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/tutti-frutti.png",
         // "title": "Essência Ziggy",
         "description": "Tutti Frutti",
-        "value": 40
+        "value": 30
     },
     {
         "img": "/assets/images/cardapio/essencias/uva.png",
         // "title": "Essência Ziggy",
         "description": "Uva",
-        "value": 40
+        "value": 30
     },
 ]
